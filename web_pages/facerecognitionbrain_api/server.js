22d2b5086918d511f0a5dbfcb690bb68c0fcf6d6
Donald <solanonaranj@hotmail.com>
2020-03-10 23:17:24 -0500

    some changes and interarion through the temporal database

--- web_pages/facerecognitionbrain_api/server.js
+++ web_pages/facerecognitionbrain_api/server.js
@@ -1,11 +1,110 @@
 const express = require('express');
+const cors = require('cors');
 const app = express();
-//const bodyParser = require('body-parser');
+const bcrypt = require('bcrypt');
+const bodyParser = require('body-parser');
 const port = 2000;
-//app.use(bodyParser);
+const saltRounds = 10;
+
+app.use(bodyParser.json());
+app.use(cors());
+
+const temporalDatabase = {
+	users : [
+		{
+			id: '123',
+			name: 'John',
+			email: 'john@gmail.com',
+			password : 'cookies',
+			entries : 0,
+			joined : new Date()
+		},
+		{
+			id: '124',
+			name: 'Sally',
+			email: 'Sally@gmail.com',
+			password : 'bananas',
+			entries : 0,
+			joined : new Date()
+		}
+	]
+}
+
 app.get('/',(request, response)=>{
-	response.send('GET request working');
+	response.send(temporalDatabase.users);
 });
+
+app.get('/profile/:id',(request, response)=>{
+	const { id } = request.params;
+	let found = false;
+	temporalDatabase.users.forEach(user => {
+		if (user.id === id) {
+			found = true;
+			return response.json(user);
+		}
+	})
+	if (!found) {
+		response.status(400).json('User not found');
+	}
+});
+
+app.post('/signin',(request, response)=>{
+	let tryerChecker = 0;
+	for (var i = 0; i < temporalDatabase.users.length; i++) {
+
+		if (request.body.email === temporalDatabase.users[i].email && request.body.password === temporalDatabase.users[i].password ) {
+			tryerChecker = 0;
+			return response.status(200).json('Success signing in');
+		} else {
+			tryerChecker++;
+			continue;
+		}
+	}
+	if (tryerChecker === temporalDatabase.users.length) {
+		tryerChecker = 0;
+		response.status(400).json('Error signing in');
+	}
+});
+
+app.post('/register',(request, response)=>{
+	const {email, name, password} = request.body;
+	temporalDatabase.users.push({
+		id: '125',
+		name: name,
+		email: email,
+		password: password,
+		entries: 0,
+		joined: new Date()
+	});
+	response.json('Successful registration');
+});
+
+app.put('/image',(request, response)=>{
+	const { id } = request.body;
+	let found = false;
+	temporalDatabase.users.forEach(user => {
+		if (user.id === id) {
+			found = true;
+			user.entries++;
+			return response.json(user.entries);
+		}
+	})
+	if (!found) {
+		response.status(400).json('User not found');
+	}
+});
+
 app.listen(port, () => {
 	console.log(`Server is running on port: ${port}`);
 });
+
+/*
+-----EndPoints-----
+/ --> res = This is working
+/signin --> POST = success/fail
+/register --> POST = user
+/profile/:userId --> GET = user
+/image --> PUT --> user 
+*/
+
+
