596cf73011b6b6311e5314d43cb076929627a138
Donald <solanonaranj@hotmail.com>
2020-05-21 16:21:21 -0500

    Requests handled with external modules for better redability

--- web_pages/facerecognitionbrain_api/server.js
+++ web_pages/facerecognitionbrain_api/server.js
@@ -1,116 +1,42 @@
+ const profileID = require('./handlers/profileID');
+const register = require('./handlers/register');
+const signin = require('./handlers/signin');
+const image = require('./handlers/image');
+const score = require('./handlers/scoreTable');
+const deleteUser = require('./handlers/deleteLoggedUser');
 const express = require('express');
 const cors = require('cors');
 const app = express();
+const knex = require('knex')
+const database = knex({
+	client: 'pg',
+	connection: {
+		host : '127.0.0.1',
+		user : 'postgres',
+		password : 'Dsolanon96',
+		database : 'smart-brain'
+	}
+});
 const bcrypt = require('bcrypt');
 const bodyParser = require('body-parser');
 const port = 2000;
-const saltRounds = 10;
 
 app.use(bodyParser.json());
 app.use(cors());
 
-const temporalDatabase = {
-	users : [
-		{
-			id: '123',
-			name: 'John',
-			email: 'john@gmail.com',
-			password : 'cookies',
-			entries : 0,
-			joined : new Date()
-		},
-		{
-			id: '124',
-			name: 'Sally',
-			email: 'Sally@gmail.com',
-			password : 'bananas',
-			entries : 0,
-			joined : new Date()
-		}
-	]
-}
+app.get('/profile/:id', (request, response) => {profileID.handleProfileID(request, response, database)});
 
-app.get('/',(request, response)=>{
-	response.send(temporalDatabase.users);
-});
+app.delete('/deleteLoggedUser', (request, response) => {deleteUser.handleDeleteLoggedUser(request, response, database)});
 
-app.get('/profile/:id',(request, response)=>{
-	const { id } = request.params;
-	let found = false;
-	temporalDatabase.users.forEach(user => {
-		if (user.id === id) {
-			found = true;
-			return response.json(user);
-		}
-	})
-	if (!found) {
-		response.status(400).json('User not found');
-	}
-});
+app.get('/scoreTable', (request, response) => {score.handleScoreTable(request, response, database)});
 
-app.post('/signin',(request, response)=>{
-	let tryerChecker = 0;
-	for (var i = 0; i < temporalDatabase.users.length; i++) {
+app.post('/signin', (request, response) => {signin.handleSignin(request, response, database, bcrypt)});
 
-		if (request.body.email === temporalDatabase.users[i].email && request.body.password === temporalDatabase.users[i].password ) {
-			tryerChecker = 0;
-			return response.status(200).json(temporalDatabase.users[i]);
-		} else {
-			tryerChecker++;
-			continue;
-		}
-	}
-	if (tryerChecker === temporalDatabase.users.length) {
-		tryerChecker = 0;
-		response.status(400).json('Error signing in');
-	}
-});
+app.post('/register', (request, response)=>{register.handleRegister(request, response, database, bcrypt)});
 
-const registerDataChecker = data => {
-	return typeof data === 'string' && data !== ''? true : false; 
-}
+app.put('/image', (request, response)=>{image.handleImage(request, response, database)});
 
-app.post('/register',(request, response)=>{
-	const {email, name, password} = request.body;
-
-	let currentUsersQuantity = temporalDatabase.users.length;
-
-	const validName = registerDataChecker(name);
-	const validEmail = registerDataChecker(email);
-	const validPassword = registerDataChecker(password);
-
-	if (validName && validEmail && validPassword) {
-		temporalDatabase.users.push({
-			id: '125',
-			name: name,
-			email: email,
-			password: password,
-			entries: 0,
-			joined: new Date()
-		});
-	} 
-
-	if (temporalDatabase.users.length === currentUsersQuantity + 1) {
-		response.status(200).json(temporalDatabase.users[temporalDatabase.users.length-1]);
-	} else {
-		response.status(400).json('Unsuccessful registration');
-	}
-});
-
-app.put('/image',(request, response)=>{
-	const { id } = request.body;
-	let found = false;
-	temporalDatabase.users.forEach(user => {
-		if (user.id === id) {
-			found = true;
-			user.entries++;
-			return response.status(200).json(user.entries);
-		}
-	})
-	if (!found) {
-		response.status(400).json('User not found');
-	}
-});
+app.post('/imageurl', (request, response)=>{image.handleApiCall(request, response)});
 
 app.listen(port, () => {
 	console.log(`Server is running on port: ${port}`);
@@ -124,5 +50,4 @@ app.listen(port, () => {
 /profile/:userId --> GET = user
 /image --> PUT --> user 
 */
-
 
